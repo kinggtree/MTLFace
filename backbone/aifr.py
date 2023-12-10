b2e6c14364ee789aeea4c78789d6f8f7d5775589
kinggtree <ghoststeve2017@outlook.com>
2023-12-10 19:18:07 +0800

    修复了 id_loss, da_loss, age_loss 训练过程中丢失的问题

--- backbone/aifr.py
+++ backbone/aifr.py
@@ -68,6 +68,8 @@ class AttentionModule(nn.Module):
         x_id = x - x_age
 
         return x_id, x_age
+    
+
 
 
 class AIResNet(IResNet):
@@ -98,36 +100,57 @@ class AIResNet(IResNet):
     
 
 class DenseNetAIFR(nn.Module):
-    def __init__(self, num_classes=512, input_size=224, drop_rate=0.4):
+    def __init__(self, input_size=224, drop_rate=0.4):
         super(DenseNetAIFR, self).__init__()
-        # 导入预训练的DenseNet
+        # 导入预训练的DenseNet121
         densenet = models.densenet121(pretrained=True)
         self.features = densenet.features
 
+        # 因为DenseNet121的特征图尺寸为7x7时特征通道数为1024
+        final_feature_map_size = (input_size // 32) ** 2
+        final_channels = 1024
+
         # 注意力模块
-        self.attention = AttentionModule(channels=1024)  # 确保通道数与DenseNet的输出匹配
+        self.attention = AttentionModule(channels=final_channels)  
 
-        # 计算DenseNet特征图在全局平均池化之前的尺寸
-        final_feature_map_size = (input_size // 32) ** 2  # DenseNet的缩减因子是32
+        # 全连接层，这里我们保持与AIResNet类似的结构
+        self.output_layer = nn.Sequential(
+            nn.BatchNorm2d(final_channels),
+            nn.Dropout(drop_rate),
+            nn.Flatten(),
+            nn.Linear(final_channels * final_feature_map_size, 512),
+            nn.BatchNorm1d(512))
 
-        # 全连接层
-        self.flatten = nn.Flatten()
-        self.dropout = nn.Dropout(p=drop_rate)
-        self.fc = nn.Linear(1024 * final_feature_map_size, num_classes)
-        self.bn = nn.BatchNorm1d(num_classes)
+        self._initialize_weights()
 
-    def forward(self, x, return_age=False):
+    def forward(self, x, return_age=False, return_shortcuts=False):
         features = self.features(x)
         x_id, x_age = self.attention(features)
-        
-        # 展平特征并应用Dropout和全连接层
-        out_id = self.fc(self.dropout(self.flatten(x_id)))
-        out_id = self.bn(out_id)
+        embedding = self.output_layer(x_id)
 
+        if return_shortcuts:
+            # 这里我们无法提供类似ResNet的shortcut，因此返回None
+            return None, None, None, None, features, x_id, x_age
         if return_age:
-            return out_id, x_id, x_age
+            return embedding, x_id, x_age
+        return embedding
 
-        return out_id
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.xavier_uniform_(m.weight.data)
+                if m.bias is not None:
+                    m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm1d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                nn.init.xavier_uniform_(m.weight.data)
+                if m.bias is not None:
+                    m.bias.data.zero_()
 
 
 
@@ -161,6 +184,6 @@ backbone_dict = {
     'ir64': partial(AIResNet, num_layers=[3, 4, 10, 3], mode="ir"),
     'ir101': partial(AIResNet, num_layers=[3, 13, 30, 3], mode="ir"),
     'irse101': partial(AIResNet, num_layers=[3, 13, 30, 3], mode="ir_se"),
-    'densenet': partial(DenseNetAIFR, num_classes=512, input_size=224)
+    'densenet': partial(DenseNetAIFR, input_size=224)
 
 }
